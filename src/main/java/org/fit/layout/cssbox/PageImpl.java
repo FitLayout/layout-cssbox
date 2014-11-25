50b3c2b64a6b529f051c7ed54fc371644509e473
Radek Burget <burgetr@fit.vutbr.cz>
2014-11-25 10:32:52 +0100

    New PageImpl based on DefaultPage

--- src/main/java/org/fit/layout/cssbox/PageImpl.java
+++ src/main/java/org/fit/layout/cssbox/PageImpl.java
@@ -6,109 +6,20 @@
 package org.fit.layout.cssbox;
 
 import java.net.URL;
-import java.util.Vector;
 
-import org.fit.layout.model.Box;
-import org.fit.layout.model.Page;
-import org.fit.layout.model.Rectangular;
+import org.fit.layout.impl.DefaultPage;
 
 /**
  * 
  * @author burgetr
  */
-public class PageImpl implements Page
+public class PageImpl extends DefaultPage
 {
-    protected URL url;
-    protected BoxNode root;
-    protected int width;
-    protected int height;
-    
 
     public PageImpl(URL url)
     {
-        this.url = url;
+        super(url);
     }
     
-    @Override
-    public URL getSourceURL()
-    {
-        return url;
-    }
-
-    @Override
-    public Box getRoot()
-    {
-        return root;
-    }
-
-    public void setRoot(BoxNode root)
-    {
-        this.root = root;
-    }
-
-    @Override
-    public int getWidth()
-    {
-        return width;
-    }
-
-    public void setWidth(int width)
-    {
-        this.width = width;
-    }
-
-    @Override
-    public int getHeight()
-    {
-        return height;
-    }
-
-    public void setHeight(int height)
-    {
-        this.height = height;
-    }
-
-    @Override
-    public Box getBoxAt(int x, int y)
-    {
-        return recursiveGetBoxAt(root, x, y);
-    }
-    
-    protected Box recursiveGetBoxAt(Box root, int x, int y)
-    {
-        if (root.getBounds().contains(x, y))
-        {
-            for (int i = 0; i < root.getChildCount(); i++)
-            {
-                Box ret = recursiveGetBoxAt(root.getChildBox(i), x, y);
-                if (ret != null)
-                    return ret;
-            }
-            return root;
-        }
-        else
-            return null;
-    }
-
-    @Override
-    public Vector<Box> getBoxesInRegion(Rectangular r)
-    {
-        Vector<Box> ret = new Vector<Box>();
-        recursiveGetBoxesInRegion(root, r, ret);
-        return ret;
-    }
-    
-    private void recursiveGetBoxesInRegion(Box root, Rectangular r, Vector<Box> result)
-    {
-        if (r.intersects(root.getBounds()))
-        {
-            result.add(root);
-        }
-        else
-        {
-            for (int i = 0; i < root.getChildCount(); i++)
-                recursiveGetBoxesInRegion(root.getChildBox(i), r, result);
-        }
-    }
     
 }
